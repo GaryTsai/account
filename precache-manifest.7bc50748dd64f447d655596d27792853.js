37248aad92ff49bafa92c20c9009dbc9f92e3b83
gary_tsai <gary_tsai@perfectcorp.com>
2020-08-23 19:05:59 +0800

    Updates

---
+++ precache-manifest.7bc50748dd64f447d655596d27792853.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c6ef6d66fb43e39ba76aa2f937ffce",
+    "url": "/rvaccount/index.html"
+  },
+  {
+    "revision": "432fc0fc49f19f954b94",
+    "url": "/rvaccount/static/css/3.775bf402.chunk.css"
+  },
+  {
+    "revision": "13d135de72a8415d6c21",
+    "url": "/rvaccount/static/css/main.c9766ebb.chunk.css"
+  },
+  {
+    "revision": "432fc0fc49f19f954b94",
+    "url": "/rvaccount/static/js/3.40049d85.chunk.js"
+  },
+  {
+    "revision": "c0f135b2e6e543c1f53a8458f16c2615",
+    "url": "/rvaccount/static/js/3.40049d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0903f2c31c3f610cd6",
+    "url": "/rvaccount/static/js/4.862cc4e4.chunk.js"
+  },
+  {
+    "revision": "c4a9d494bbad818f4f8f",
+    "url": "/rvaccount/static/js/5.d972b167.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/rvaccount/static/js/5.d972b167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d074a2e0424600b8fc6",
+    "url": "/rvaccount/static/js/6.851195cc.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/rvaccount/static/js/6.851195cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d135de72a8415d6c21",
+    "url": "/rvaccount/static/js/main.9a2847c4.chunk.js"
+  },
+  {
+    "revision": "555a269ec0a0d4f0bd32",
+    "url": "/rvaccount/static/js/runtime-main.1e5dd661.js"
+  },
+  {
+    "revision": "fdc9b26f74e89f45e760",
+    "url": "/rvaccount/static/js/xlsx.562f03f1.chunk.js"
+  },
+  {
+    "revision": "73abf0126abe075d13d9bc7873690a36",
+    "url": "/rvaccount/static/media/loading.73abf012.gif"
+  }
+]);
