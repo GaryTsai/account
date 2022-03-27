413bf0ea5fcd1527d0066e707523f79609979f3d
gary\user <garybird606@gmail.com>
2022-03-27 18:41:03 +0800

    fix route test[14]

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -6,7 +6,7 @@ const utils = {
       date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
       expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/account/";
   },
   getCookie: (name) => {
     var nameEQ = name + "=";
